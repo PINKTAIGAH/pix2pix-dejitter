ce3ff0f7fa315b8f4d0bb7dd093fd826c6882986
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-08-18 16:07:33 +0100

    Added comments for config file

--- src/config.py
+++ src/config.py
@@ -5,13 +5,45 @@ from torch.utils.tensorboard.writer import SummaryWriter
 from torchvision.transforms import transforms as transform
 
 
-def normalise(x):
+def _normalise(x):
+    """
+    Normalise a ndArray
+
+    Parameters
+    ----------
+    x: ndArray
+        Input array containing dataset to be normalised
+
+    Returns
+    -------
+    output: ndArray
+        Normalised array
+    """
     if np.sum(x) == 0:
         raise Exception("Divided by zero. Attempted to normalise a zero tensor")
 
     return x/np.sum(x**2)
 
+def _generatePointFunction():
+    """
+    Generate a image array of a point function with size of defined in config 
+    file (excluding padding)
+    
+    Returns
+    -------
+    pointFunction: ndArray
+        2D image array containing a point function
+    """
+
+    pointFunction = np.zeros((NOISE_SIZE, NOISE_SIZE))
+    pointFunction[NOISE_SIZE//2, NOISE_SIZE//2] = 1
+    return pointFunction
+
+"""
+Hyper Parameters
+"""
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# Directory of files containing image datasets
 TRAIN_DIR = "/home/giorgio/Desktop/p06_images/train/"
 # TRAIN_DIR = "/home/brunicam/myscratch/p3_scratch/p06_images/train/"
 VAL_DIR = "/home/giorgio/Desktop/p06_images/val/"
@@ -19,14 +51,15 @@ VAL_DIR = "/home/giorgio/Desktop/p06_images/val/"
 LEARNING_RATE = 2e-4
 BATCH_SIZE = 32
 SCHEDULAR_DECAY = 0.5
-SCHEDULAR_PATIENCE = 20
+SCHEDULAR_STEP = 20                         # Step size of learning rate schedular
+OPTIMISER_WEIGHTS = (0.5, 0.999)            # Beta parameters of Adam optimiser
 NUM_WORKERS = 2
 MAX_JITTER = 4
 PADDING_WIDTH = 30
 IMAGE_SIZE = 256 
 NOISE_SIZE = IMAGE_SIZE - PADDING_WIDTH*2
-SIGMA = 20
-CHANNELS_IMG = 1 
+SIGMA = 20                                  # Standard deviation of gaussian kernal for PSF
+CHANNELS_IMG = 1                            # Colour channels of input image tensors 
 L1_LAMBDA = 100
 LAMBDA_GP = 10
 CORRELATION_LENGTH = 10
@@ -38,9 +71,9 @@ CHECKPOINT_GEN = "gen.pth.tar"
 # WRITER_REAL = SummaryWriter("/home/brunicam/myscratch/p3_scratch/runs/real")
 # WRITER_FAKE = SummaryWriter("/home/brunicam/myscratch/p3_scratch/runs/fake")
 
-kernal = np.zeros((NOISE_SIZE, NOISE_SIZE))
-kernal[NOISE_SIZE//2, NOISE_SIZE//2] = 1
-PSF = torch.from_numpy(normalise(gaussian(kernal, SIGMA)))
+# Generate a point spread finction by applying a gaussian blur to a point function
+pointFunction = _generatePointFunction()
+PSF = torch.from_numpy(_normalise(gaussian(pointFunction, SIGMA))).type(torch.float32)
 
 transforms = transform.Compose([
     transform.Normalize(
@@ -49,7 +82,7 @@ transforms = transform.Compose([
     ),
 ])
 
-transformsCell = transform.Compose([
+transformsFile = transform.Compose([
     transform.ToTensor(),
     transform.RandomCrop(IMAGE_SIZE),
     transform.Grayscale(),
