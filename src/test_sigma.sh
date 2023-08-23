d7852488a79f6964be5664ab164cf195757f9295
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-08-23 13:05:52 +0100

    changed bash script

--- src/test_sigma.sh
+++ src/test_sigma.sh
@@ -5,6 +5,6 @@ for i in ${sigma_array[@]}; do
     echo "SIGMA = $i" >> config.py
     echo "EVALUATION_IMAGE_FILE= '../evaluation/sigma_$i'" >> config.py
     mkdir ../evaluation/sigma_$i
-    # python3 train.py 
-    # python3 evaluation.py
+    python3 train.py 
+    python3 evaluation.py
 done
